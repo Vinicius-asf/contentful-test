35422115a345947a777e9a205da0a2c1996457d3
Vinicius Ferreira <viniciusant3@gmail.com>
2022-01-17 17:42:53 -0300

    testing contentful api

---
+++ content_fetch.js
@@ -0,0 +1,31 @@
+const contentfulData = {
+    url:'https://cdn.contentful.com',
+    spaces:'42jcfhpme8t6',
+    entriesLocation:'b4eR_MZ5KS8yEQg_Ov0Ta8WhXkul5GyS0DlATqE5Xik'
+}
+
+const contentfulURL = `${contentfulData.url}/spaces/${contentfulData.spaces}/entries?access_token=${contentfulData.entriesLocation}`;
+
+const documentMain = document.getElementById('main')
+
+const contentfulFetch = fetch(contentfulURL).then(async response => {
+    if (response.ok) {
+        const postJson = await response.json();
+        // console.log(postJson)
+        postJson.items.forEach(post=>{
+            const title = document.createElement("h1");
+            // const description = document.createElement("p");
+            title.innerHTML = post.fields.title;
+            // description.innerHTML = post.description
+            documentMain.appendChild(title)
+        })
+        postJson.includes.Asset.forEach(asset => {
+            console.log(asset)
+            const img = document.createElement('img');
+            img.src = asset.fields.file.url;
+            documentMain.appendChild(img)
+        })
+    } else {
+        alert("HTTPS-Error:" + response.status);
+    }
+});
